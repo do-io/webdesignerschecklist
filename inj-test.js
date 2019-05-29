8661bfb1df372360e7704bad1772c69bec22a579
Darren <darren@vinoshipper.com>
2019-05-29 15:31:23 -0700

    window?

--- inj-test.js
+++ inj-test.js
@@ -708,7 +708,7 @@ window.onload = (function(window) {
       if (document.getElementsByClassName('vs-add-to-cart')) {
           vsAddToCartButton.init('.vs-add-to-cart', 1);
       }
-      document.addEventListener('message',onMessage);
+      window.addEventListener('message',onMessage);
     });
   }
 })(window);
